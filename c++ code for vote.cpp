ab96023d5c19b2e9a65c0458a5021afd75f03f27
sumedh-875 <66565487+sumedh-875@users.noreply.github.com>
2020-09-12 17:06:41 +0530

    eligible for voting programme
    by Sumedh Pathrudkar

---
+++ c++ code for vote.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int age;
+	cout<<"enter age of user:";
+	cin>>age;
+	
+	if(age>=18)
+	{
+		cout<<"you are eligible";
+    }
+    else
+    {
+    	cout<<"\n you are not eligible";
+	}
+}
